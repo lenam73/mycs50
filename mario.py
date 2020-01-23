2828fc916bb066b2cc75b8a5e4a83153fe9a7c62
amozgovoy <alexandr@mozgovoy.com>
2020-01-23 02:54:17 -0500

    fixed the loops range and added type check exception

--- mario.py
+++ mario.py
@@ -1,18 +1,21 @@
 import sys
 
-# while True:
-#     n = int(input("Height: "))
-#     if 1 <= n <= 8:
-#         break
-n = 8
-for i in range(1, n):
+while True:
+    try:
+        n = int(input("Height: "))
+    except ValueError:
+        continue
+    if 1 <= n <= 8:
+        break
+
+for i in range(1, n + 1):
     k = n - i
-    for j in range(1, n):
+    for j in range(1, n + 1):
         if j > k:
             sys.stdout.write('#')
         else:
             sys.stdout.write(" ")
     sys.stdout.write('  ')
-    for j in range(1, i):
+    for j in range(1, i + 1):
         sys.stdout.write('#')
     sys.stdout.write("\n")
